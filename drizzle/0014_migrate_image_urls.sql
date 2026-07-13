9192957f95114a5c850f19ace5c4b963f435ec87
Logan Reeder <148260300+logscore@users.noreply.github.com>
2026-07-12 18:34:11 -0600

    Update 0014_migrate_image_urls.sql

--- drizzle/0014_migrate_image_urls.sql
+++ drizzle/0014_migrate_image_urls.sql
@@ -4,8 +4,8 @@ WHERE `image` LIKE '/images/%';--> statement-breakpoint
 UPDATE `organization`
 SET `logo` = CONCAT('/api', `logo`)
 UPDATE `media`
-SET `poster_url` = '/api' || `poster_url`
+SET `poster_url` = CONCAT('/api', `poster_url`)
 WHERE `poster_url` LIKE '/images/%';--> statement-breakpoint
 UPDATE `media`
-SET `backdrop_url` = '/api' || `backdrop_url`
+SET `backdrop_url` = CONCAT('/api', `backdrop_url`)
 WHERE `backdrop_url` LIKE '/images/%';
